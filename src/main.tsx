cb8f8977f8ee8eca4cf80448d520afe7c691990f
Luke NB <luke@0is1.com>
2023-09-29 13:42:03 -0400

    no sleep

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
 import { MicrophoneProvider } from "./contexts/MicrophoneContext";
 
+// Prevent the screen from going to sleep
+import NoSleep from "@marsgames/nosleep.js";
+const noSleep = new NoSleep();
+document.addEventListener(
+  "click",
+  function enableNoSleep() {
+    document.removeEventListener("click", enableNoSleep, false);
+    noSleep.enable();
+  },
+  false
+);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
