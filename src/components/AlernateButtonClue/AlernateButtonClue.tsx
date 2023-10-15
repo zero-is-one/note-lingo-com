823c20d59a36cec962b402a999a094ab6811827f
Luke NB <luke@0is1.com>
2023-10-15 18:29:20 -0400

    front back icons

--- src/components/AlernateButtonClue/AlernateButtonClue.tsx
+++ src/components/AlernateButtonClue/AlernateButtonClue.tsx
@@ -1,7 +1,8 @@
 import { Flashcard, InstrumentBehavior } from "@/types";
 import { angloConcertinaCgWheatstoneInstrument } from "@/config/instruments/angloConcertinaCgWheatstone";
+import { RxTextAlignBottom, RxTextAlignTop } from "react-icons/rx";
 
-import { Heading } from "@chakra-ui/react";
+import { HStack, Heading, Icon } from "@chakra-ui/react";
 export const AlernateButtonClue = ({ flashcard }: { flashcard: Flashcard }) => {
   const hasNoteConflict = angloConcertinaCgWheatstoneInstrument.buttons.some(
     (button, index) =>
@@ -29,7 +30,11 @@ export const AlernateButtonClue = ({ flashcard }: { flashcard: Flashcard }) => {
 
   return (
     <>
-      <Heading>{position}</Heading>
+      <HStack>
+        {position === "Back" && <Icon boxSize={10} as={RxTextAlignBottom} />}
+        <Heading>{position}</Heading>
+        {position === "Front" && <Icon boxSize={10} as={RxTextAlignTop} />}
+      </HStack>
     </>
   );
 };
